c8f3c9e7aa47db11f3b0ba7719ae4bc9d8d8b3aa
Haris Mohanty <harismohanty8658@gmail.com>
2023-11-23 12:17:01 +0530

    server- database/db.js- Mongodb database connected successfully.

--- server/database/db.js
+++ server/database/db.js
@@ -1,1 +1,17 @@
-import moduleName from 'mongoose'
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+
+    console.log(
+      `Connected to MongoDb Database ${mongoose.connect.host}.`.bgBrightGreen
+        .black
+    );
+  } catch (err) {
+    console.log(`MongoDb Database Error ${err}.`.bgBrightRed.white);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
