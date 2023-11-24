066b3435e824073ecce844024c993eb023f78e4d
Haris Mohanty <harismohanty8658@gmail.com>
2023-11-24 08:26:49 +0530

    client- src/components/Header/Header.jsx- logo added.

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import './HeaderStyles.css'
+import React from "react";
+import "./HeaderStyles.css";
+import { AppBar, Box, Toolbar } from "@mui/material";
+// import { Link } from "react-router-dom";
+import logo from "../../Assets/amazon.png";
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <AppBar component={"nav"}>
+        <Toolbar>
+          <Box className="left">
+            <Box width={"14%"}>
+              {/* <Link to={"/"}> */}
+                <img src={logo} alt="mern amazon logo" width={"100%"} />
+              {/* </Link> */}
+            </Box>
+          </Box>
+          <Box className="right"></Box>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
 
-export default Header
+export default Header;
